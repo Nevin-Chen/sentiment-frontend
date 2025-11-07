85afd3d1ee243e37a81886f65462095bdf8221b6
nevin-chen <nevinchen6@gmail.com>
2025-11-06 20:22:21 -0500

    Install auth0-react

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import FlashProvider from "./context/FlashContext";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <FlashProvider>
-      <App />
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{ redirect_uri: window.location.origin + "/callback"}}
+      >
+        <Router>
+          <App />
+        </Router>
+      </Auth0Provider>
     </FlashProvider>
   </StrictMode>
 );
